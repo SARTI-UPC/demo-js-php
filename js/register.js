fbd444c15c1b3601ca97a75920cb284f0f7faeeb
Antoni Oller Arcas <aollera@uoc.edu>
2024-04-16 12:05:24 +0200

    exemple password robust

--- js/register.js
+++ js/register.js
@@ -12,3 +12,15 @@ let comarques = [
         poblacions: ["Sitges", "Vilanova i la Geltrú"]
     }
 ];
+
+function getStrongLevel(password) {
+    var level = 0;
+    level += password.length > 6 ? 1 : 0;
+    level += /[!@#$%^&*?_~]{2,}/.test(password) ? 1 : 0;
+    level += /[a-z]{2,}/.test(password) ? 1 : 0;
+    level += /[A-Z]{2,}/.test(password) ? 1 : 0;
+    level += /[0-9]{2,}/.test(password) ? 1 : 0;
+    return level;
+}
+
+
