d2949398a5a237597e963c0f24a999850596e1c9
Antoni Oller Arcas <aollera@uoc.edu>
2024-04-22 11:22:58 +0200

    upload & download de ficheros

--- afegeixProducte.php
+++ afegeixProducte.php
@@ -1,9 +1,14 @@
 <?php
+
 $name = $_POST["name"];
 $category = $_POST["category"];
 $price = $_POST["price"];
 $id = $_POST["id"];
 $operation = $_POST["operation"];
+
+print_r($_POST);
+echo "<P><p>Dades sobre fitxers";
+print_r($_FILES);
 
 
 echo "name: $name <br>";
@@ -11,6 +16,14 @@ echo "category: $category <br>";
 echo "price: $price <br>";
 echo "id: $id <br>";
 echo "operation: $operation <br>";
+
+$src =  $_FILES['imatge']['tmp_name'];
+$dst = "/tmp/".$_FILES['imatge']['name'];
+
+echo "<p>origen $src <br>";
+echo "<p>desti $dst <br>";
+
+move_uploaded_file( $_FILES['imatge']['tmp_name'], $dst);
 
 
 ?>
