ac86eb870cade80469614aeb89d845591f28b62d
Antoni Oller Arcas <aollera@uoc.edu>
2024-04-29 19:23:59 +0200

    script de servidor per upload de fitxers

---
+++ files/uploadFile.php
@@ -0,0 +1,13 @@
+<?php
+print_r($_FILES);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
+        $archivo_nombre = $_FILES['file']['name'];
+        echo "arxiu '$archivo_nombre'  rebut correctament.";
+    } else {
+        echo "Error al rebre l'arxiu.";
+    }
+} else {
+    echo "Operació només disponible per el mètode 'POST'.";
+}
+?>
